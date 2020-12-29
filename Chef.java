1a2e29e42411482d64f5c4576705b6368fff4141
Will Buerger <william_buerger@brown.edu>
2020-12-29 10:22:12 -0500

    Stencil

--- Chef.java
+++ Chef.java
@@ -8,12 +8,4 @@ public class Chef {
     public void makePizza(String topping) {
         System.out.println("Making pizza with " + topping);
     }
-
-    public void cutPizza(int numPieces) {
-        System.out.println("Cutting pizza into " + numPieces + " slices");
-    }
-
-    public int howManySlices(int numPizzas, int slicesPerPizza) {
-        return numPizzas * slicesPerPizza;
-    }
 }
